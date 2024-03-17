64312799ee50494e93a949f3c1ea8a62e38f2da4
taka16-tnk <taka16.tanaka@gmail.com>
2024-03-17 20:25:29 +0900

    Initial Commit

---
+++ AsWebAuth App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsWebAuth App
+//
+//  Created by たなかたかひろ on 2024/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
